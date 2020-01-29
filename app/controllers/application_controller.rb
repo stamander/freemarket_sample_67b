506879e771bb2440c6257970d822e387ec3af3b5
stamander <iiida.atsushi.fh@gmail.com>
2020-01-29 14:56:34 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '67_b' && password == '0313'
+    end
+  end
 end
