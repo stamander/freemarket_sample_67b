87a166f45feb609b28b4e808492dcdc423a87263
stamander <iiida.atsushi.fh@gmail.com>
2020-02-05 16:25:09 +0900

    routesとcontroller

--- app/controllers/exhibition_controller.rb
+++ app/controllers/exhibition_controller.rb
@@ -1,2 +1,13 @@
 class ExhibitionController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "top#index"
   resources :top, only: :index 
-  resources :exhibition, only: :index 
+  resources :exhibition, only: [:index,:create,:new,:update] 
   resources :itempurchase, only: :index
 end
