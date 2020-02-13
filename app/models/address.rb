5a019230523210d0229228f13984ab561ce0897d
akotaso <oimo.moimo@gmail.com>
2020-02-13 20:29:25 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture :way_of_delively :category_id :quality :postage :shipping_date
+  belongs_to_active_hash :prefecture, :way_of_delively, :category_id, :quality, :postage, :shipping_date
 end
