d2fc126181180dc24d0b037ef0e00d24aca2a100
arataka33 <59165182+arataka33@users.noreply.github.com>
2020-02-17 13:04:41 +0900

    手直し

--- app/controllers/detail_controller.rb
+++ app/controllers/detail_controller.rb
@@ -1,4 +1,30 @@
 class DetailController < ApplicationController
   def index
+    
+  end
+
+  def  destroy
+    @detail.destroy
+    redirect_to root_path
+  end
+
+  # def destroy
+  #   if @item.user_id == current_user.id
+  #     @item.destroy
+  #     redirect_to users_path
+  #   else
+  #     render :show,notice: '削除出来ませんでした'
+  #   end
+  # end
+
+  def update
+    if @detail.update(detail_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
   end
 end
