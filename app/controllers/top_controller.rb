c5060cd17f81b7cef00f20158fc4dc86f0bb226c
akotaso <oimo.moimo@gmail.com>
2020-02-17 10:59:43 +0900

    Update top_controller.rb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,8 +1,8 @@
 class TopController < ApplicationController
   
   def index
-    @item = Item.all.where(SaleStatu: "販売中").order(id: :desc).includes(:images)
-    @categoryitem = Item.all.where(SaleStatu: "販売中").where(category_id: "1").order(id: :desc)
+    @item = Item.all.order(id: :desc).includes(:images)
+    @categoryitem = Item.all.where(category_id: "1").order(id: :desc)
     @image = Image.all.order(id: :desc).limit 3
   end
 
