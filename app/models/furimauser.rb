9f90e9f65c6120aca4b6c8fcd5f74960fa39a7d3
stamander <iiida.atsushi.fh@gmail.com>
2020-02-19 20:18:11 +0900

    buyer_id　終了

--- app/models/furimauser.rb
+++ app/models/furimauser.rb
@@ -5,4 +5,11 @@ class Furimauser < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :cards
   has_many :items
+
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "furimauser_id", class_name: "Item"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "furimauser_id", class_name: "Item"
+
+
+
 end
